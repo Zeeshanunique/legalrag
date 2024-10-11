f0515620505333db7ca48c1d506eda7f9f11ae71
Zeeshanunique <zeeshanunique2619@email.com>
2024-10-11 14:44:34 +0530

    chainging icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,7 @@ const source_code_pro = Source_Code_Pro({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Kanoon",
   description: "The law-school dropout with a solid advice",
+  icons:['/favicon.ico'],
 };
 
 export default function RootLayout({
